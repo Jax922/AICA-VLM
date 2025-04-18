7818ee79634d6f7f01c94172c2a6b6df6e4c8b1f
Tony <2983523460@qq.com>
2025-04-18 18:03:19 +0800

    updata FI process file

--- data_processing/FI.py
+++ data_processing/FI.py
@@ -2,38 +2,38 @@ import os
 import csv
 import re
 
-# 指定图片文件夹路径
+# Specify the path to the image folder
 image_folder = "FI_dataset_images"
-# 指定输出CSV文件路径
+# Specify the path to the output CSV file
 output_csv = "FI_dataset_annotations.csv"
 
-# 创建CSV文件
+# Create a CSV file
 with open(output_csv, 'w', newline='') as csvfile:
-    # 定义CSV表头
+    # Define the CSV header
     fieldnames = ['img_name', 'img_folder', 'emotion_cat', 'emotion_v', 'emotion_a', 'dataset_source']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     
-    # 写入表头
+    # Write the header
     writer.writeheader()
     
-    # 遍历文件夹中的所有图片
+    # Iterate through all the images in the folder
     for filename in os.listdir(image_folder):
         if filename.lower().endswith('.jpg'):
-            # 使用正则表达式从文件名中提取标签（第一个下划线前的内容）
+            # Use regular expressions to extract the label from the filename (the content before the first underscore)
             match = re.match(r"([^_]+)_", filename)
             
             if match:
                 emotion_category = match.group(1)
                 
-                # 创建并写入一行数据
+                # Create and write a line of data
                 writer.writerow({
                     'img_name': filename,
                     'img_folder': 'emotion_dataset_images',
                     'emotion_cat': emotion_category,
-                    'emotion_v': '',  # 空值
-                    'emotion_a': '',  # 空值
+                    'emotion_v': '',  
+                    'emotion_a': '', 
                     'dataset_source': 'FI_dataset'
                 })
 
-print(f"CSV文件已创建: {output_csv}")
+print(f"CSV file has been created : {output_csv}")
 print(f"CSV processing completed successfully.")
